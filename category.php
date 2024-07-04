2672ae36893df00cb1366e488b23df5fa7f3893d
Shiv Nandan <36573508+ShivNandan-Coder@users.noreply.github.com>
2024-07-04 08:15:44 +0530

    Update category.php

--- category.php
+++ category.php
@@ -1,2 +1,16 @@
-[{
-}]
+[
+  {
+    "name": "Category 1",
+    "items": [
+      { "item_name": "Item 1" },
+      { "item_name": "Item 2" }
+    ]
+  },
+  {
+    "name": "Category 2",
+    "items": [
+      { "item_name": "Item 3" },
+      { "item_name": "Item 4" }
+    ]
+  }
+]
